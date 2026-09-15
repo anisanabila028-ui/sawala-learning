674f6cc9d66802bdaacc64390ce244fcccac151c
anisanabila028 <anisanabila028@gmail.com>
2026-09-15 09:46:45 +0700

    perbaikan

--- 02-async-api-typescript/02-fetch-api/index.js
+++ 02-async-api-typescript/02-fetch-api/index.js
@@ -11,3 +11,42 @@ fetch('https://jsonplaceholder.typicode.com/users')
 .catch((error) => {
     console.log( "terjadi kesalahan ", error.message)
 });
+
+//get
+const response = await fetch ("https://jsonplacholder.typicode.com/users");
+const data = await response.json();
+console.log(data);
+
+//POST
+const postData = await fetch("https://jsonplaceholder.typicode.com/users", {
+    method: "POST",
+    headers: {
+        "Content-Type": "application/json"
+    },
+    body: JSON.stringify({
+        name: "annisa nabila",
+        email: "annisa.nb@example.com"
+    })
+});
+const newData = await postData.json();
+console.log(newData);
+
+//PUT 
+const putData = await fetch("https://jsonplaceholder.typicode.com/users", {
+    method: "PUT",
+    headers: {
+        "Content-Type": "application/json"
+    },
+    body: JSON.stringify({
+        name: "annisa",
+        email: "annisa.nb@example.com"
+    })
+});
+const Data = await putData.json();
+console.log(Data);
+
+//DELETE
+const deleteData = await fetch ("https://jsonplacholder.typicode.com/users", {
+    method: "DELETE"
+});
+console.log("berhasil dihapus");
